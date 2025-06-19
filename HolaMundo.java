235f46d254636d1ed5c1dd46e374861c9c448ccf
Itzy Jareth Valle Mendoza <itzy.valle349@std.eni.edu.ni>
2025-06-19 12:37:29 -0600

    Primer commit: programa HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+public class HolaMundo {
+    public static void main(String[] args){
+
+        String nombre = "Estudiantes";
+
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+
+        System.out.println("Hila," + nombre + "!");
+        System.out.println("La suma de "+ a +" + " + b +"es:" +suma);
+    }
+}
